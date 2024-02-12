8ca10de9d35cf9720201a3e8ea8e3acc6d71592d
dulranga <dulranga.webdesign@gmail.com>
2024-02-12 17:18:35 +0530

    updated links

--- README.md
+++ README.md
@@ -34,8 +34,8 @@ Users should be able to:
 
 ### Links
 
-- Solution URL: [Add solution URL here](https://your-solution-url.com)
-- Live Site URL: [Add live site URL here](https://your-live-site-url.com)
+- Solution URL: [https://github.com/dulranga/Multi-Step-Form](https://github.com/dulranga/Multi-Step-Form)
+- Live Site URL: [https://multi-step-form-dulranga.vercel.app](https://multi-step-form-dulranga.vercel.app/)
 
 ## My process
 
--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,28 @@ import GetInfo from "./pages/GetInfo";
 
 function App() {
   return (
-    <main className="min-h-screen bg-neutral-300 flex justify-center items-center">
+    <main className="min-h-screen bg-neutral-300 grid place-items-center p-5">
       <GetInfo />
+
+      <div className="font-body">
+        Challenge by&nbsp;
+        <a
+          href="https://www.frontendmentor.io?ref=challenge"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Frontend Mentor. &nbsp;
+        </a>
+        Coded by &nbsp;
+        <a
+          href="https://github.com/dulranga"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="underline font-bold"
+        >
+          Dulranga Dhawanitha
+        </a>
+      </div>
     </main>
   );
 }
